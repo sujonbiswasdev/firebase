00e0f2c4bb21016a17a2cdfe7b84cc34d50792bc
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-10-05 14:25:45 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className='p-5 container mx-auto '>
       <nav className='py-28 mt-16 bg-slate-400'>
-        <ul className='flex items-center space-x-4 justify-center'>
+        <ul className='flex flex-wrap   space-y-6 md:items-center space-x-4 md:justify-center'>
           <li ><Link to="/register" className='px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-800'>Register</Link></li>
           <li ><Link to="/login" className='px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-800'>Login</Link></li>
           <li ><Link to="/dashboard" className='px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-800'>dashboard</Link></li>
